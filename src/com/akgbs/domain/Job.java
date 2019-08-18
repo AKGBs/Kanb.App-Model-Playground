0db1ef8a7ea06425c22bad28ba4d16033be36ede
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-08-18 15:16:14 +0200

    Added job test for constructor

--- src/com/akgbs/domain/Job.java
+++ src/com/akgbs/domain/Job.java
@@ -1,9 +1,33 @@
 package com.akgbs.domain;
 
+import java.util.ArrayList;
+
 public class Job {
     /*
      * Group of tasks that are logically grouped to execute a job
      */
+
+    private final String name;
+    private final String description;
+    private final ArrayList<Task> taskArray;
+
+    public Job(String name, String description, ArrayList<Task> taskArray) {
+        this.taskArray = taskArray;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ArrayList<Task> getTaskArray() {
+        return taskArray;
+    }
 
 
 }
--- src/com/akgbs/tests/JobTest.java
+++ src/com/akgbs/tests/JobTest.java
@@ -1,20 +1,42 @@
-package com.akgbs.domain;
+package com.akgbs.tests;
 
+import com.akgbs.domain.Job;
+import com.akgbs.domain.Task;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Given a job")
 class JobTest {
 
-    @Test
-    void getName() {
+    private static ArrayList<Task> taskArray;
+    private static String name;
+    private static String description;
+
+    @BeforeAll
+    public static void settings() {
+        name = "Name";
+        description = "Description";
+        taskArray = new ArrayList<Task>(Stream
+                .of(new Task(), new Task())
+                .collect(Collectors.toUnmodifiableList()));
     }
 
     @Test
-    void getDescription() {
+    @DisplayName("When created, check attribute getters ")
+    void test_getters() {
+        Job job = new Job(name, description, taskArray);
+
+        assertEquals(name, job.getName());
+        assertEquals(description, job.getDescription());
+        assertEquals(taskArray, job.getTaskArray());
     }
 
-    @Test
-    void getTaskArray() {
-    }
 }
