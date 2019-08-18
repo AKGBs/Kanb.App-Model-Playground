b1b06831f4a876f24c3921c53159175521f1a986
Bugli Lorenzo <bugli.lorenzo@gmail.com>
2019-08-18 15:16:07 +0200

    Added job test for constructor

---
+++ src/com/akgbs/tests/JobTest.java
@@ -0,0 +1,20 @@
+package com.akgbs.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JobTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getTaskArray() {
+    }
+}
